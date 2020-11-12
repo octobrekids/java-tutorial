06f91e8c4216bfe842a23cc43352758ed358f4d7
octobrekids <chnrd.k@gmail.com>
2020-11-12 15:04:41 +0700

    Add radiobutton group

--- GUI01/src/guiFrame/App.java
+++ GUI01/src/guiFrame/App.java
@@ -8,6 +8,22 @@ public class App {
         // Create new frame from SimpleFrame
         SimpleFrame window = new SimpleFrame("Hello World");
 
+        // declare radiobutton group
+        ButtonGroup group = new ButtonGroup();
+
+        JRadioButton r1 = new JRadioButton("Programmer", false);
+        JRadioButton r2 = new JRadioButton("Accountant", false);
+        JRadioButton r3 = new JRadioButton("Engineer", false);
+
+        // add radio to group
+        group.add(r1);
+        group.add(r2);
+        group.add(r3);
+
+        // declare label
+        JLabel label = new JLabel();
+        label.setText("Hello ka");
+
         // declare button
         JButton button1 = new JButton("OK");
         JButton button2 = new JButton("Save");
@@ -15,8 +31,14 @@ public class App {
         // set layout from left to right and at the center
         window.setLayout(new FlowLayout());
 
+        window.add(label);
         window.add(button1);
         window.add(button2);
+
+        // add radio to frame
+        window.add(r1);
+        window.add(r2);
+        window.add(r3);
 
         // set close operation -> minimize, expand, close
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
