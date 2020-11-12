5de38c1f7dc05b7a651d996a2dbe8a54e41a549e
octobrekids <chnrd.k@gmail.com>
2020-11-12 14:59:18 +0700

    Add JButton

--- GUI01/src/guiFrame/App.java
+++ GUI01/src/guiFrame/App.java
@@ -1,11 +1,22 @@
 package guiFrame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class App {
     public static void main(String[] args) {
         // Create new frame from SimpleFrame
-        SimpleFrame window = new SimpleFrame();
+        SimpleFrame window = new SimpleFrame("Hello World");
+
+        // declare button
+        JButton button1 = new JButton("OK");
+        JButton button2 = new JButton("Save");
+
+        // set layout from left to right and at the center
+        window.setLayout(new FlowLayout());
+
+        window.add(button1);
+        window.add(button2);
 
         // set close operation -> minimize, expand, close
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- GUI01/src/guiFrame/SimpleFrame.java
+++ GUI01/src/guiFrame/SimpleFrame.java
@@ -4,12 +4,19 @@ import javax.swing.*;
 
 // inherit from JFrame to use the method
 public class SimpleFrame extends JFrame {
+
     // declare width, height
     public static final int WIDTH=500;
     public static final int HEIGHT=400;
+
     // Constructor
     SimpleFrame() {
         setTitle("Hello");
+        setSize(WIDTH,HEIGHT);
+    }
+
+    public SimpleFrame(String title){
+        super(title);
         setSize(WIDTH,HEIGHT);
     }
 }
